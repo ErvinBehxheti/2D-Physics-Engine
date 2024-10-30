c559bef530252eb4571ff3ea14e46ac34e57fa92
ErvinBehxheti <ervinbehxheti05@gmail.com>
2024-10-30 13:19:57 +0100

    Drew a circle

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function drawBall(x, y, r) {
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.strokeStyle = "black";
+  ctx.stroke();
+  ctx.fillStyle = "red";
+  ctx.fill();
+}
